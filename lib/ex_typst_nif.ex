8da7a0d1fa2debf37bfddd8b1bf057c5ae52c6b8
William Lanchantin <william.lanchantin@cargosense.com>
2025-10-28 11:14:28 -0400

    First pass at using RustlerPrecompiled

--- lib/ex_typst_nif.ex
+++ lib/ex_typst_nif.ex
@@ -1,7 +1,14 @@
 defmodule ExTypst.NIF do
   @moduledoc false
+  version = Mix.Project.config()[:version]
 
-  use Rustler, otp_app: :ex_typst, crate: "extypst_nif"
+  # use Rustler, otp_app: :ex_typst, crate: "extypst_nif"
+  use RustlerPrecompiled,
+    otp_app: :ex_typst,
+    crate: "extypst_nif",
+    base_url: "https://github.com/billylanchantin/ex_typst/releases/download/v#{version}",
+    force_build: System.get_env("RUSTLER_PRECOMPILED_BUILD") in ["1", "true"],
+    version: version
 
   def compile(_content, _font_paths), do: :erlang.nif_error(:nif_not_loaded)
 end
