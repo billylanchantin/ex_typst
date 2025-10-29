2c29121620ad97a0ca487e78e0e57e16e03bc2fc
William Lanchantin <william.lanchantin@cargosense.com>
2025-10-29 10:40:20 -0400

    More nuance to download link

--- lib/ex_typst_nif.ex
+++ lib/ex_typst_nif.ex
@@ -1,12 +1,20 @@
 defmodule ExTypst.NIF do
   @moduledoc false
-  version = Mix.Project.config()[:version]
+
+  mix_config = Mix.Project.config()
+  version = mix_config[:version]
+  github_url = mix_config[:package][:links]["GitHub"]
+  tag = "v#{version}"
+  tag = "rustler-precompiled-test-2"
+  # Since Rustler 0.27.0, we need to change manually the mode for each env.
+  # We want "debug" in dev and test because it's faster to compile.
+  mode = if Mix.env() in [:dev, :test], do: :debug, else: :release
 
   # use Rustler, otp_app: :ex_typst, crate: "extypst_nif"
   use RustlerPrecompiled,
     otp_app: :ex_typst,
     crate: "extypst_nif",
-    base_url: "https://github.com/billylanchantin/ex_typst/releases/download/v#{version}",
+    base_url: "#{github_url}/releases/download/#{tag}",
     force_build: System.get_env("RUSTLER_PRECOMPILED_BUILD") in ["1", "true"],
     version: version
 
