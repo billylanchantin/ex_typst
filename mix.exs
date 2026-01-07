578fdda221376dc2cf82a7e7d44c674ce9cddd46
William Lanchantin <william.lanchantin@cargosense.com>
2026-01-07 14:22:09 -0500

    Add CargoSense versioning

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ExTypst.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/billylanchantin/ex_typst/tree/rustler-precompiled-cargosense"
-  @version "0.1.3"
+  @version "0.1.3-cargosense.3"
 
   def project do
     [
